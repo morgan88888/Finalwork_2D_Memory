007e4db8f5693d9fdaa8e2d0af73a4012a0bcad2
morgan88888 <55180693+morgan88888@users.noreply.github.com>
2020-12-23 21:27:51 +0800

    場景建置

---
+++ Finalwork_2D_Memory/Assets/script/Interact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject Button;
+    public GameObject talkUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Button.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Button.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Button.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        {
+            talkUI.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            talkUI.SetActive(false);
+        }
+    }
+}
