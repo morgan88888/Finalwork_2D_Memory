6366fbe9c4a85b1534c5f6c3d35319a4874d7460
hurbie <55180721+hurbie@users.noreply.github.com>
2021-04-08 00:06:33 +0800

    CH01final?

---
+++ Finalwork_2D_Memory/Assets/script/Chapter01/day2/onlySIS03.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class onlySIS03 : MonoBehaviour
+{
+    public GameObject Button;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Button.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Button.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (Button.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("onlySIS03");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Button.SetActive(false);
+        }
+    }
+}
