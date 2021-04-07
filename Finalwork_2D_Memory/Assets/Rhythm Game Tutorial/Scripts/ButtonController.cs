0d468ca7d54450b17ea767c1dd21e8f60a45fff2
morgan88888 <55180693+morgan88888@users.noreply.github.com>
2021-04-08 02:50:45 +0800

    小遊戲1完成

---
+++ Finalwork_2D_Memory/Assets/Rhythm Game Tutorial/Scripts/ButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+   
+    private SpriteRenderer theSR;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode KeyToPress;
+    
+    void Start(){
+        theSR = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyToPress))
+        {
+            theSR.sprite = pressedImage;
+        }
+
+        if(Input.GetKeyUp(KeyToPress))
+        {
+            theSR.sprite = defaultImage;
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Finalwork_2D_Memory/Assets/Rhythm Game Tutorial/Scripts/Trap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+
+
+{
+    public int life;
+    public GameObject GAMEOVER;
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("箭頭"))
+        {
+            life--;
+        }
+        if (life >= 5)
+        {
+            GAMEOVER.SetActive(true);
+        }
+        
+    }
+
+}
