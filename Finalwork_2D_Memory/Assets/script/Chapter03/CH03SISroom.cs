2f673b5ef0b0b4ca3de70f5d6ce60619f7da6ca8
hurbie <55180721+hurbie@users.noreply.github.com>
2021-04-27 00:14:02 +0800

    對話結束Vol.1

---
+++ Finalwork_2D_Memory/Assets/script/Chapter03/CH03SISroom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CH03SISroom : MonoBehaviour
+{
+    public GameObject Button;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Button.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Button.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (Button.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("CH03SISroom");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Button.SetActive(false);
+        }
+    }
+}
